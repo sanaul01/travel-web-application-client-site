936258345529a09b97cc1c2afe674feb62c5c103
Md. Sanaul Islam <86657683+sanaul01@users.noreply.github.com>
2022-01-27 23:57:45 +0600

    banner updated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,7 +3,7 @@ import React from "react";
 const Banner = () => {
   return (
     <div>
-      <h2>This is banner</h2>
+      <img style={{width: '100%'}} src="https://s3.amazonaws.com/cdn.designcrowd.com/blog/2018/May/5-Steps-to-Designing-an-Eye-Catching-Travel-Blog/DI_5-Steps-To-Designing-An-Eye-Catching-Travel-Blog_Banner_828x300.jpg" alt="" />
     </div>
   );
 };
