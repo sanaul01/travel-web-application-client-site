529f7673b87ff78e0564f0503ae2b55589b434b2
Md. Sanaul Islam <86657683+sanaul01@users.noreply.github.com>
2022-01-28 17:38:05 +0600

    footer added

--- src/Pages/Home/Footer/Footer.js
+++ src/Pages/Home/Footer/Footer.js
@@ -1,10 +1,35 @@
+import { Grid, Typography } from "@mui/material";
 import React from "react";
+import { blueGrey, orange } from "@mui/material/colors";
 
 const Footer = () => {
   return (
-    <div>
-      <h2>This is footer</h2>
-    </div>
+    <Grid container spacing={2} sx={{mt: 8, backgroundColor: blueGrey[800], color: orange[700] }}>
+      <Grid item xs={12} sm={12} style={{display: 'flex'}}>
+        <Grid sm={3}>
+          <Typography>travel@blogs.com</Typography>
+          <Typography>www.TravelBlogs.com</Typography>
+          <Typography>www.facebooke.com/ReavelsBlogs</Typography>
+        </Grid>
+        <Grid sm={3}>
+          <Typography>Mobail: +088 19 333 333 22</Typography>
+          <Typography>Phone: +088 19 333 333 22</Typography>
+        </Grid>
+        <Grid sm={3}>
+        <Typography>Mobail: +088 19 333 333 22</Typography>
+          <Typography>Phone: +088 19 333 333 22</Typography>
+        </Grid>
+        <Grid sm={3}>
+        <Typography>Mobail: +088 19 333 333 22</Typography>
+          <Typography>Phone: +088 19 333 333 22</Typography>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} sm={12}>
+      <p>
+        &copy;{new Date().getFullYear()} City Guide App -All Rights Reserved
+      </p>
+      </Grid>
+    </Grid>
   );
 };
 
