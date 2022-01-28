97b2bb86576cdb70cc75143c4e0e1d266603910d
Md. Sanaul Islam <86657683+sanaul01@users.noreply.github.com>
2022-01-28 17:44:01 +0600

    update footer

--- src/Pages/Home/Footer/Footer.js
+++ src/Pages/Home/Footer/Footer.js
@@ -4,8 +4,12 @@ import { blueGrey, orange } from "@mui/material/colors";
 
 const Footer = () => {
   return (
-    <Grid container spacing={2} sx={{mt: 8, backgroundColor: blueGrey[800], color: orange[700] }}>
-      <Grid item xs={12} sm={12} style={{display: 'flex'}}>
+    <Grid
+      container
+      spacing={2}
+      sx={{ mt: 8, backgroundColor: blueGrey[800], color: orange[700] }}
+    >
+      <Grid item xs={12} sm={12} style={{ display: "flex" }}>
         <Grid sm={3}>
           <Typography>travel@blogs.com</Typography>
           <Typography>www.TravelBlogs.com</Typography>
@@ -16,18 +20,18 @@ const Footer = () => {
           <Typography>Phone: +088 19 333 333 22</Typography>
         </Grid>
         <Grid sm={3}>
-        <Typography>Mobail: +088 19 333 333 22</Typography>
+          <Typography>Mobail: +088 19 333 333 22</Typography>
           <Typography>Phone: +088 19 333 333 22</Typography>
         </Grid>
         <Grid sm={3}>
-        <Typography>Mobail: +088 19 333 333 22</Typography>
+          <Typography>Mobail: +088 19 333 333 22</Typography>
           <Typography>Phone: +088 19 333 333 22</Typography>
         </Grid>
       </Grid>
       <Grid item xs={12} sm={12}>
-      <p>
-        &copy;{new Date().getFullYear()} City Guide App -All Rights Reserved
-      </p>
+        <p>
+          &copy;{new Date().getFullYear()} City Guide App -All Rights Reserved
+        </p>
       </Grid>
     </Grid>
   );
