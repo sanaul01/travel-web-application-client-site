03a56ef831f4ed178eee3d394bb00ead50409d77
Md. Sanaul Islam <86657683+sanaul01@users.noreply.github.com>
2022-01-28 20:49:00 +0600

    Ui updated in blogs page

--- src/Pages/Home/AllBlogs/AllBlogs.js
+++ src/Pages/Home/AllBlogs/AllBlogs.js
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { lightGreen } from "@mui/material/colors";
@@ -24,43 +24,71 @@ const AllBlogs = () => {
   }, [page]);
 
   return (
-    <Box sx={{ flexGrow: 1, mt: 8 }}>
-      <Grid
-        container
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 4, sm: 8, md: 12 }}
-      >
-        {blogs?.map((blog) => (
-          <Grid item xs={2} sm={4} md={4} key={blog._id}>
-            <img style={{ width: "50%" }} src={blog.image} alt="" />
-            <Typography>{blog.title}</Typography>
-            <Typography>{blog.description}</Typography>
-            <NavLink
-              style={{ textDecoration: "none" }}
-              to={`/details/${blog._id}`}
+    <Container>
+      <Box sx={{ flexGrow: 1, mt: 8 }}>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          {blogs?.map((blog) => (
+            <Grid
+              item
+              xs={2}
+              sm={4}
+              md={4}
+              key={blog._id}
+              sx={{ "&:hover": { boxShadow: 3 } }}
             >
-              <Button
-                sx={{ my: 3, backgroundColor: lightGreen[700] }}
-                variant="contained"
+              <img
+                style={{ width: "90%", height: "250px", borderRadius: "10px" }}
+                src={blog.image}
+                alt=""
+              />
+              <Typography
+                style={{
+                  fontWeight: 700,
+                  marginBottom: "20px",
+                }}
               >
-                Details
-              </Button>
-            </NavLink>
-          </Grid>
-        ))}
-      </Grid>
-      <Grid className="pagination">
-        {[...Array(pageCount).keys()].map((number) => (
-          <button
-            className={number === page ? "selected" : ""}
-            key={number}
-            onClick={() => setPage(number)}
-          >
-            {number}
-          </button>
-        ))}
-      </Grid>
-    </Box>
+                {blog.title}
+              </Typography>
+              <Typography
+                style={{
+                  textAlign: "left",
+                }}
+              >
+                {blog.description.slice(0, 150)}
+              </Typography>
+              <NavLink
+                style={{
+                  textDecoration: "none",
+                }}
+                to={`/details/${blog._id}`}
+              >
+                <Button
+                  sx={{ my: 3, backgroundColor: lightGreen[700] }}
+                  variant="contained"
+                >
+                  Details
+                </Button>
+              </NavLink>
+            </Grid>
+          ))}
+        </Grid>
+        <Grid className="pagination" sx={{ mt: 8 }}>
+          {[...Array(pageCount).keys()].map((number) => (
+            <button
+              className={number === page ? "selected" : ""}
+              key={number}
+              onClick={() => setPage(number)}
+            >
+              {number}
+            </button>
+          ))}
+        </Grid>
+      </Box>
+    </Container>
   );
 };
 
