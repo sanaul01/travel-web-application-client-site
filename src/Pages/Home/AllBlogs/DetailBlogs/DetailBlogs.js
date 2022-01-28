50e7500876e26c40005914b0e1f45d9ae02d5273
Md. Sanaul Islam <86657683+sanaul01@users.noreply.github.com>
2022-01-28 16:51:14 +0600

    detail page updated

--- src/Pages/Home/AllBlogs/DetailBlogs/DetailBlogs.js
+++ src/Pages/Home/AllBlogs/DetailBlogs/DetailBlogs.js
@@ -1,49 +1,36 @@
-import { Grid, Typography } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Grid, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const DetailBlogs = () => {
-    const {blogId} = useParams();
-    const [blog, setBlog] = useState();
+  const { blogId } = useParams();
+  const [blog, setBlog] = useState();
 
-
-    useEffect(()=>{
-        fetch(`https://peaceful-sea-14435.herokuapp.com/blogs/${blogId}`)
-        .then(res =>res.json())
-        .then(data => setBlog(data))
-    } , [])
-    console.log(blog)
-    return (    
-            <Grid container spacing={2}>
-                <Grid item xs={12} sm={12}>
-                    <Grid item xs={12} sm={6}>
-                        <Typography>
-                            <img style={{width: '100%'}} src={blog?.image} alt="" />
-                        </Typography>
-                    </Grid>
-                    <Grid xs={12} sm={6}>
-                        <Typography>
-                            {blog?.title}
-                        </Typography>
-                        <Typography>
-                            {blog?.loaction}
-                        </Typography>
-                        <Typography>
-                            {blog?.info}
-                        </Typography>
-                        <Typography>
-                            {blog?.category}
-                        </Typography>
-                        <Typography>
-                            ${blog?.cost}
-                        </Typography>
-                        <Typography>
-                            ${blog?.description}
-                        </Typography>
-                    </Grid>
-                </Grid>
-            </Grid>
-    );
+  useEffect(() => {
+    fetch(`https://peaceful-sea-14435.herokuapp.com/blogs/${blogId}`)
+      .then((res) => res.json())
+      .then((data) => setBlog(data));
+  }, []);
+  console.log(blog);
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={12}>
+        <Grid item xs={12} sm={6}>
+          <Typography>
+            <img style={{ width: "100%" }} src={blog?.image} alt="" />
+          </Typography>
+        </Grid>
+        <Grid xs={12} sm={6}>
+          <Typography>{blog?.title}</Typography>
+          <Typography>{blog?.loaction}</Typography>
+          <Typography>{blog?.info}</Typography>
+          <Typography>{blog?.category}</Typography>
+          <Typography>${blog?.cost}</Typography>
+          <Typography>${blog?.description}</Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default DetailBlogs;
