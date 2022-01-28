a8489f4a175bad879802c4a0e29829b2b0600c87
Md. Sanaul Islam <86657683+sanaul01@users.noreply.github.com>
2022-01-28 16:47:27 +0600

    ui updated

--- src/Pages/Home/AllBlogs/AllBlogs.js
+++ src/Pages/Home/AllBlogs/AllBlogs.js
@@ -1,28 +1,30 @@
 import { Box, Button, Grid, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { lightGreen } from "@mui/material/colors";
 
 const AllBlogs = () => {
-    const [blogs, setBlogs] = useState();
-    const [pageCount, setPageCount] = useState(0);
-    const [page, setPage] = useState(0);
+  const [blogs, setBlogs] = useState();
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
 
+  const size = 9;
 
-    const size = 10;
-
-    useEffect(()=>{
-        fetch(`https://peaceful-sea-14435.herokuapp.com/blogs?page=${page}&&size=${size}`)
-        .then(res => res.json())
-        .then(data => {
-          setBlogs(data.blogs)
-          const count = data.count;
-          const pageNumber = Math.ceil(count / size);
-          setPageCount(pageNumber);
-        })
-    } , [page])
+  useEffect(() => {
+    fetch(
+      `https://peaceful-sea-14435.herokuapp.com/blogs?page=${page}&&size=${size}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setBlogs(data.blogs);
+        const count = data.count;
+        const pageNumber = Math.ceil(count / size);
+        setPageCount(pageNumber);
+      });
+  }, [page]);
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1, mt: 8 }}>
       <Grid
         container
         spacing={{ xs: 2, md: 3 }}
@@ -30,30 +32,34 @@ const AllBlogs = () => {
       >
         {blogs?.map((blog) => (
           <Grid item xs={2} sm={4} md={4} key={blog._id}>
-              <img style={{width: '50%'}} src={blog.image} alt="" />
-              <Typography>
-                  {blog.title}
-              </Typography>
-              <Typography>
-                  {blog.description}
-              </Typography>
-              <NavLink to={`/details/${blog._id}`}>
-                <Button>Details</Button>
-              </NavLink>
+            <img style={{ width: "50%" }} src={blog.image} alt="" />
+            <Typography>{blog.title}</Typography>
+            <Typography>{blog.description}</Typography>
+            <NavLink
+              style={{ textDecoration: "none" }}
+              to={`/details/${blog._id}`}
+            >
+              <Button
+                sx={{ my: 3, backgroundColor: lightGreen[700] }}
+                variant="contained"
+              >
+                Details
+              </Button>
+            </NavLink>
           </Grid>
         ))}
       </Grid>
       <Grid className="pagination">
-          {[...Array(pageCount).keys()].map((number) => (
-            <button
-              className={number === page ? "selected" : ""}
-              key={number}
-              onClick={() => setPage(number)}
-            >
-              {number}
-            </button>
-          ))}
-        </Grid>
+        {[...Array(pageCount).keys()].map((number) => (
+          <button
+            className={number === page ? "selected" : ""}
+            key={number}
+            onClick={() => setPage(number)}
+          >
+            {number}
+          </button>
+        ))}
+      </Grid>
     </Box>
   );
 };
--- src/Pages/Home/Navigation/Navigation.js
+++ src/Pages/Home/Navigation/Navigation.js
@@ -6,8 +6,9 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
+import { lightGreen } from "@mui/material/colors";
 
 const Navigation = () => {
   const { user, logOut } = useAuth();
@@ -15,18 +16,18 @@ const Navigation = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar sx={{backgroundColor: lightGreen[700] }}>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
+            sx={{ mr: 2}}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Travelar
+            Travel Blogs
           </Typography>
 
           {user?.email ? (
